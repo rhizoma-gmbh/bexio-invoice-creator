1edc48ac2636491304a5e2db01e9c690d24ffdd7
Sebastian Martinez <me@sebastinez.dev>
2023-07-17 08:32:00 +0200

    Adding an Invoice class and start with sdk
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
-import { request } from "./lib.ts";
-import { createInvoiceRequestObject } from "./lib/invoice.ts";
+
+import * as schema from "./lib/schema.ts";
+import { Invoice } from "./lib/invoice.ts";
+import { Output, formatText } from "./lib/klog.ts";
+import { request } from "./lib/request.ts";
 
 const decoder = new TextDecoder();
 const groups: Record<string, number> = {};
 
 for await (const chunk of Deno.stdin.readable) {
-  // TODO: Handle possible partial chunk.
   const obj: Output = JSON.parse(decoder.decode(chunk));
 
   obj.records.forEach((record) => {
@@ -21,16 +23,48 @@ for await (const chunk of Deno.stdin.readable) {
   });
 }
 
-const invoices = Object.entries(groups).map(([text, amount]) => ({
-  amount: (amount / 60).toFixed(2),
-  text: formatText(text),
-}));
+const positions = Object.entries(groups).map<schema.PositionCreate>(
+  ([text, amount]) => ({
+    amount: (amount / 60).toFixed(2),
+    unit_id: 2,
+    account_id: 150,
+    tax_id: 16,
+    text: formatText(text),
+    unit_price: "75",
+    discount_in_percent: "0",
+    type: "KbPositionCustom",
+  })
+);
+
+const invoice = Invoice.create({
+  title: "",
+  contact_id: 8,
+  contact_sub_id: null,
+  user_id: 1,
+  pr_project_id: null,
+  language_id: 4,
+  bank_account_id: 1,
+  currency_id: 1,
+  payment_type_id: 4,
+  header: "",
+  footer: "",
+  mwst_type: 0,
+  mwst_is_net: false,
+  show_position_taxes: false,
+  is_valid_from: "",
+  is_valid_to: "",
+  reference: "",
+  api_reference: "",
+  template_slug: "",
+  positions: [],
+}).addPositions(positions);
 
 try {
   const response = await request(
     "/2.0/kb_invoice",
     "POST",
-    createInvoiceRequestObject(invoices)
+    {},
+    invoice.toJSON()
   );
   if (response.ok) {
     console.log("");
--- lib/invoice.ts
+++ lib/invoice.ts
@@ -1,35 +1,22 @@
-export function createInvoiceRequestObject(
-  positions: { amount: string; text: string }[]
-) {
-  return {
-    title: "",
-    contact_id: 2, // Radicle Foundation
-    contact_sub_id: null,
-    user_id: 1, // Sebastian Martinez
-    pr_project_id: null,
-    language_id: 4, // English
-    bank_account_id: 1, // Raiffeisen Account
-    currency_id: 1, // CHF
-    payment_type_id: 4, // Rechnung
-    header: "",
-    footer: "",
-    mwst_type: 0,
-    mwst_is_net: false,
-    show_position_taxes: false,
-    is_valid_from: "",
-    is_valid_to: "",
-    reference: "",
-    api_reference: "",
-    template_slug: "",
-    positions: positions.map(({ amount, text }) => ({
-      amount,
-      unit_id: 2, // unit hours
-      account_id: 150, // Dienstleistungserlös
-      tax_id: 16, // 7.7%
-      text,
-      unit_price: "75",
-      discount_in_percent: "0",
-      type: "KbPositionCustom",
-    })),
-  };
+import type * as schema from "./schema.ts";
+
+export class Invoice {
+  #invoice: schema.InvoiceCreate;
+
+  private constructor(invoice: schema.InvoiceCreate) {
+    this.#invoice = invoice;
+  }
+
+  public static create(invoice: schema.InvoiceCreate) {
+    return new Invoice(invoice);
+  }
+
+  public addPositions(position: schema.PositionCreate[]) {
+    this.#invoice.positions.push(...position);
+    return this;
+  }
+
+  public toJSON() {
+    return this.#invoice;
+  }
 }
