32b1cee87b0d6a3e38da2c59715dc3234ee8146f
Sebastian Martinez <me@sebastinez.dev>
2023-07-17 08:26:16 +0200

    Move `request` to own module and add querystring support
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- lib/request.ts
+++ lib/request.ts
@@ -1,11 +1,17 @@
 const baseUrl = "https://api.bexio.com";
 
-export async function request<T>(
+export async function request<T, R>(
   pathname: string,
   method: string,
+  // deno-lint-ignore no-explicit-any
+  query: Record<string, any> = {},
   body?: T
 ): Promise<Response> {
-  return await fetch(`${baseUrl}${pathname}`, {
+  const params = new URLSearchParams(query).toString();
+  const url = new URL(
+    `${baseUrl}${pathname}${params ? `?${params.toString()}` : ""}`
+  );
+  return await fetch(url, {
     method,
     body: body ? JSON.stringify(body) : undefined,
     headers: {
